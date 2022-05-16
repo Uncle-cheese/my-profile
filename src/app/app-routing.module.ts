07fc0181d0478db58fde4e7dffd09e245082669b
Uncle cheese <nhlanhla.mavundla@dadevs.co.za>
2022-05-16 12:33:46 +0200

    profile not done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'portfolio',
+    loadChildren: () => import('./pages/portfolio/portfolio.module')
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'portfolio',
     pathMatch: 'full'
   },
   {
-    path: 'portfolio',
+    path: 'home',
     loadChildren: () => import('./pages/portfolio/portfolio.module').then( m => m.PortfolioPageModule)
   },
 ];
